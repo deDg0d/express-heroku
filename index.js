0062d0dec98130922a48d1287d068389dc13f174
dani <danielramezany@yahoo.com>
2021-03-21 21:24:27 +0430

    fg

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
 const express = require ('express')
 const mongoose = require('mongoose')
 const app = express()
-
+const port = process.env.PORT || 3000;
 app.use(express.urlencoded({extended:true}))
 
 
 //connecting
-// mongoose.connect('mongodb+srv://deD-god:Pf1571378@cluster0.jxvjj.mongodb.net/node?retryWrites=true&w=majority', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true
-//   })
-//   .then((result)=>{app.listen(8080)
-//     console.log('db connected')})
-//     //creating
-//   const Schema = mongoose.Schema
-//   const DataType = new Schema({
-//   title: String,
-//   body: String,
-//   date: Date  
-//   } , {timestamps:true})
-//   const Web = mongoose.model('Web', DataType) 
+mongoose.connect(process.env.MONGODB_URI ||'mongodb+srv://deD-god:Pf1571378@cluster0.jxvjj.mongodb.net/node?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then((result)=>{app.listen(port)
+    console.log('db connected')})
+    //creating
+  const Schema = mongoose.Schema
+  const DataType = new Schema({
+  title: String,
+  body: String,
+  date: Date  
+  } , {timestamps:true})
+  const Web = mongoose.model('Web', DataType) 
   
-const port = process.env.PORT || 3000;
+
 
 app.get('/',function(req,res){
   res.send('server is live')
   console.log('server is running')
  })
- app.listen(port)
-// // app.get('/add',(req,res)=>{
-// //     const web = new Web({ 
-// //         title : 'new blog',
-// //         body: ' more about',
-// //         date : 5
-// //     })
-// //     //saving
-// //     web.save()
-// //     .then((result)=>{res.send(result)})
-// //     .catch((err)=>console.log(err))
-// // })
-// app.get('/load',(req,res)=>{
-//     Web.find()
-//     .then((result)=>res.send(result))
-//     .catch((err)=>console.log(err))
-// }) 
-// app.get('/delete/:id',(req,res)=>{
-//   const id =req.params.id
-//   Web.findByIdAndDelete(id)
-//   .catch((err)=> {throw err})
-// })
+ 
+ 
+app.get('/add',(req,res)=>{
+    const web = new Web({ 
+        title : 'new blog',
+        body: ' more about',
+        date : 5
+    })
+    //saving
+    web.save()
+    .then((result)=>{res.send(result)})
+    .catch((err)=>console.log(err))
+})
+app.get('/load',(req,res)=>{
+    Web.find()
+    .then((result)=>res.send(result))
+    .catch((err)=>console.log(err))
+}) 
+app.get('/delete/:id',(req,res)=>{
+  const id =req.params.id
+  Web.findByIdAndDelete(id)
+  .then(()=>res.send('requested item deleted'))
+  .catch((err)=> {throw err})
+})
 
